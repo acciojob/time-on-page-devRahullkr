5c0d2bb0aa3b70844620017aae6ff9736d2d3047
Rahulllkr <rahulkumar8604745@gmail.com>
2026-01-09 16:22:49 +0530

    check

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,18 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
 
-  
+  const [timer,setTimer] = useState(0)
+  setInterval(() => {
+    setTimer(timer+1)
+  },1000)
 
   return (
     <div>
         {/* Do not remove the main div */}
-        <h1>You've been on this page for {} seconds.</h1>
+        <p>You've been on this page for {timer} seconds.</p>
     </div>
   )
 }
